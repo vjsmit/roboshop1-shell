27b9ce04c0bf4c2c28d9980b9175db49fff2c811
vjsmit <vjsmit97@gmail.com>
2024-02-13 21:39:37 +0530

    updating path for frontend conf

--- frontend.sh
+++ frontend.sh
@@ -1,19 +1,20 @@
 echo -e "\e[33m Installing Nginx \e[0m"
-dnf install nginx -y
+dnf install nginx -y            &>>/tmp/roboshop.log
 
 echo -e "\e[33m Removing default html content \e[0m"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/*    &>>/tmp/roboshop.log
 
 echo -e "\e[33m Download frontend content \e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip   &>>/tmp/roboshop.log
 
 echo -e "\e[33m Unzip frontend content \e[0m"
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+cd /usr/share/nginx/html  &>>/tmp/roboshop.log
+unzip /tmp/frontend.zip   &>>/tmp/roboshop.log
 
 echo -e "\e[33m Copy reverse proxy config \e[0m"
-cp frontend.conf /etc/nginx/default.d/roboshop.conf
+cp /root/roboshop1-shell/frontend.conf /etc/nginx/default.d/roboshop.conf  &>>/tmp/roboshop.log
+
 
 echo -e "\e[33m Enable and restart nginx server \e[0m"
-systemctl enable nginx
-systemctl restart nginx
+systemctl enable nginx      &>>/tmp/roboshop.log
+systemctl restart nginx     &>>/tmp/roboshop.log
--- mongodb.sh
+++ mongodb.sh
@@ -1,12 +1,12 @@
 echo -e "\e[33m Copy mongdb repo file \e[0m"
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 
 echo -e "\e[33m Installing MongoDB \e[0m"
-dnf install mongodb-org -y
+dnf install mongodb-org -y &>>/tmp/roboshop.log
 
 echo -e "\e33m Open mongodb service for all"
-sed -i '\s\127.0.0.1\0.0.0.0\' /etc/mongod.conf
+sed -i '\s\127.0.0.1\0.0.0.0\' /etc/mongod.conf &>>/tmp/roboshop.log
 
 echo -e "\e[33m Restating mongodb service"
-systemctl enable mongod
-systemctl restart mongod
+systemctl enable mongod &>>/tmp/roboshop.log
+systemctl restart mongod &>>/tmp/roboshop.log
