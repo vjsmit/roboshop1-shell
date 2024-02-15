57eaa4b72845809764e453a7ee0a9d4867724f25
vjsmit <vjsmit97@gmail.com>
2024-02-15 21:54:00 +0530

    setting up mysql

---
+++ mysql.sh
@@ -0,0 +1,16 @@
+echo -e "\e[31m disable MySQL 8 version \e[0m"
+dnf module disable mysql -y
+
+
+echo -e "\e[31m Setup the MySQL5.7 repo file \e[0m"
+cp /home/centos/roboshop1-shell/mysql.repo /etc/yum.repos.d/mysql.repo
+
+echo -e "\e[31m Install MySQL Server \e[0m"
+dnf install mysql-community-server -y
+
+echo -e "\e[31m Start MySQL Service \e[0m"
+systemctl enable mysqld
+systemctl restart mysqld
+
+echo -e "\e[31m change the default root password \e[0m"
+mysql_secure_installation --set-root-pass RoboShop@1
