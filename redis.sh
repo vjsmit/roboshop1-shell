811025ec81072589ec8454f7e9fa709827fa08c6
vjsmit <vjsmit97@gmail.com>
2024-02-15 21:18:15 +0530

    redis installation

--- redis.sh
+++ redis.sh
@@ -1,14 +1,14 @@
 echo -e "\e[31m Creating repo file \e[0m"
-dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y    &>>/tmp/roboshop.log
 
 echo -e "\e[31m Enable Redis 6.2 \e[0m"
-dnf module enable redis:remi-6.2 -y
+dnf module enable redis:remi-6.2 -y   &>>/tmp/roboshop.log
 
 echo -e "\e[31m  Installing Redis \e[0m"
-dnf install redis -y
+dnf install redis -y    &>>/tmp/roboshop.log
 echo -e "\e[31m Update redis listen address \e[0m"
-sed -i '/s/127.0.0.1/0.0.0.0/' /etc/redis.conf  /etc/redis/redis.conf
+sed -i '/s/127.0.0.1/0.0.0.0/' /etc/redis.conf  /etc/redis/redis.conf   &>>/tmp/roboshop.log
 
 echo -e "\e[31m Restarting Redis \e[0m"
-systemctl enable redis
-systemctl restart redis
+systemctl enable redis    &>>/tmp/roboshop.log
+systemctl restart redis   &>>/tmp/roboshop.log
