f2ff3c98f470255daee454b688a9ef917231d752
vjsmit <vjsmit97@gmail.com>
2024-02-15 21:25:09 +0530

    redis installation

--- redis.sh
+++ redis.sh
@@ -7,7 +7,7 @@ dnf module enable redis:remi-6.2 -y   &>>/tmp/roboshop.log
 echo -e "\e[31m  Installing Redis \e[0m"
 dnf install redis -y    &>>/tmp/roboshop.log
 echo -e "\e[31m Update redis listen address \e[0m"
-sed -i '/s/127.0.0.1/0.0.0.0/' /etc/redis.conf  /etc/redis/redis.conf   &>>/tmp/roboshop.log
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf  /etc/redis/redis.conf   &>>/tmp/roboshop.log
 
 echo -e "\e[31m Restarting Redis \e[0m"
 systemctl enable redis    &>>/tmp/roboshop.log
