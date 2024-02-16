0ca60fbb1c5cb4b6f3852fc9150af26edac9641d
vjsmit <vjsmit97@gmail.com>
2024-02-16 18:30:26 +0530

    creating common file

--- catalogue.sh
+++ catalogue.sh
@@ -1,9 +1,5 @@
+source common.sh
 component=catalogue
-
-color="\e[35m"
-nocolor="\e[0m"
-log_file="/tmp/roboshop.log"
-app_path="/app"
 
 echo -e "${color}Disabling Default nodejs${no_color}"
 dnf module disable nodejs -y    &>>${log_file}
---
+++ common.sh
@@ -0,0 +1,4 @@
+color="\e[35m"
+nocolor="\e[0m"
+log_file="/tmp/roboshop.log"
+app_path="/app"
