1f0269fec9017cffd4e04da91df0ed0ee5f9034c
vjsmit <vjsmit97@gmail.com>
2024-02-17 10:33:36 +0530

    DRY

--- common.sh
+++ common.sh
@@ -3,16 +3,7 @@ nocolor="\e[0m"
 log_file="/tmp/roboshop.log"
 app_path="/app"
 
-func_nodejs() {
-  echo -e "${color}Disabling Default nodejs${no_color}"
-  dnf module disable nodejs -y    &>>${log_file}
-
-  echo -e "${color}Enable nodejs 18 version${no_color}"
-  dnf module enable nodejs:18 -y    &>>${log_file}
-
-  echo -e "${color}Installing nodejs${no_color}"
-  dnf install nodejs -y   &>>${log_file}
-
+app_presetup() {
   echo -e "${color}Adding roboshop user${no_color}"
   useradd roboshop    &>>${log_file}
 
@@ -27,17 +18,36 @@ func_nodejs() {
   cd ${app_path}
   unzip /tmp/${component}.zip    &>>${log_file}
 
-  echo -e "${color}Download dependencies${no_color}"
-  cd ${app_path}
-  npm install &>>${log_file}
+}
 
-  echo -e "${color}Setup systemd ${component} service${no_color}"
+func_systemd() {
+  echo -e "${color}Setup SystemD ${component} Service${no_color}"
   cp /home/centos/roboshop1-shell/${component}.service /etc/systemd/system/${component}.service   &>>${log_file}
 
   echo -e "${color}Start ${component} service${no_color}"
   systemctl daemon-reload   &>>${log_file}
   systemctl enable ${component}    &>>${log_file}
   systemctl restart ${component}     &>>${log_file}
+}
+
+
+func_nodejs() {
+  echo -e "${color}Disabling Default nodejs${no_color}"
+  dnf module disable nodejs -y    &>>${log_file}
+
+  echo -e "${color}Enable nodejs 18 version${no_color}"
+  dnf module enable nodejs:18 -y    &>>${log_file}
+
+  echo -e "${color}Installing nodejs${no_color}"
+  dnf install nodejs -y   &>>${log_file}
+
+  app_presetup
+
+  echo -e "${color}Download dependencies${no_color}"
+  cd ${app_path}
+  npm install &>>${log_file}
+
+  func_systemd
 }
 
 func_mongodb() {
@@ -63,87 +73,39 @@ func_maven() {
   echo -e "${color}Install maven${no_color}"
   dnf install maven -y    &>>${log_file}
 
-  echo -e "${color}Add application User${no_color}"
-  useradd roboshop    &>>${log_file}
-
-  echo -e "${color}Setup an app directory${no_color}"
-  rm -rf ${app_path}   &>>${log_file}
-  mkdir ${app_path}    &>>${log_file}
-
-  echo -e "${color}Download the application code & unzip to created app directory${no_color}"
-  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip   &>>${log_file}
-  cd ${app_path}
-  unzip /tmp/${component}.zip   &>>${log_file}
+  app_presetup
 
   echo -e "${color}Download the dependencies & build the application${no_color}"
   mvn clean package   &>>${log_file}
   mv target/${component}-1.0.jar ${component}.jar   &>>${log_file}
 
-  echo -e "${color}Setup SystemD ${component} Service${no_color}"
-  cp /home/centos/roboshop1-shell/${component}.service /etc/systemd/system/${component}.service   &>>${log_file}
-
   func_mysql
 
-  echo -e "${color}Start the service ${no_color}"
-  systemctl daemon-reload   &>>${log_file}
-  systemctl enable ${component}   &>>${log_file}
-  systemctl restart ${component}    &>>${log_file}
+  func_systemd
 }
 
 python() {
   echo -e "${color}Install Python 3.6${no_color}"
   dnf install python36 gcc python3-devel -y   &>>${log_file}
 
-  echo -e "${color}Add application User${no_color}"
-  useradd roboshop    &>>${log_file}
-
-  echo -e "${color}setup an app directory${no_color}"
-  rm -rf ${app_path}   &>>${log_file}
-  mkdir ${app_path}    &>>${log_file}
-
-  echo -e "${color}Download the app code and unzip${no_color}"
-  curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip   &>>${log_file}
-  cd ${app_path}
-  unzip /tmp/payment.zip    &>>${log_file}
+  app_presetup
 
   echo -e "${color}Download the dependencies${no_color}"
   pip3.6 install -r requirements.txt    &>>${log_file}
 
-  echo -e "${color}Setup SystemD Payment Service${no_color}"
-  cp /home/centos/roboshop1-shell/payment.service /etc/systemd/system/payment.service   &>>${log_file}
-
-  echo -e "${color}Start the service${no_color}"
-  systemctl daemon-reload   &>>${log_file}
-  systemctl enable payment    &>>${log_file}
-  systemctl restart payment   &>>${log_file}
+  func_systemd
 }
 
 func_golang() {
   echo -e "${color}Install GoLang${no_color}"
   dnf install golang -y   &>>${log_file}
 
-  echo -e "${color}Add application User${no_color}"
-  useradd roboshop    &>>${log_file}
-
-  echo -e "${color}Setup an app directory${no_color}"
-  rm -rf ${app_path
-  mkdir ${app_path    &>>${log_file}
-
-  echo -e "${color}Download & extract the application code to created app directory${no_color}"
-  curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip   &>>${log_file}
-  cd ${app_path
-  unzip /tmp/dispatch.zip   &>>${log_file}
+  app_presetup
 
   echo -e "${color}Download the dependencies${no_color}"
   go mod init dispatch    &>>${log_file}
   go get    &>>${log_file}
   go build    &>>${log_file}
 
-  echo -e "${color}Setup SystemD Payment Service${no_color}"
-  cp /home/centos/roboshop1-shell/dispatch.service /etc/systemd/system/dispatch.service   &>>${log_file}
-
-  echo -e "${color}Load the service ${no_color}"
-  systemctl daemon-reload   &>>${log_file}
-  systemctl enable dispatch   &>>${log_file}
-  systemctl restart dispatch    &>>${log_file}
+  func_systemd
 }
--- dispatch.sh
+++ dispatch.sh
@@ -14,20 +14,20 @@ rm -rf ${app_path
 mkdir ${app_path    &>>${log_file}
 
 echo -e "${color}Download & extract the application code to created app directory${no_color}"
-curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip   &>>${log_file}
+curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip   &>>${log_file}
 cd ${app_path
-unzip /tmp/dispatch.zip   &>>${log_file}
+unzip /tmp/${component}.zip   &>>${log_file}
 
 echo -e "${color}Download the dependencies${no_color}"
-go mod init dispatch    &>>${log_file}
+go mod init ${component}    &>>${log_file}
 go get    &>>${log_file}
 go build    &>>${log_file}
 
 echo -e "${color}Setup SystemD Payment Service${no_color}"
-cp /home/centos/roboshop1-shell/dispatch.service /etc/systemd/system/dispatch.service   &>>${log_file}
+cp /home/centos/roboshop1-shell/${component}.service /etc/systemd/system/${component}.service   &>>${log_file}
 
 echo -e "${color}Load the service ${no_color}"
 systemctl daemon-reload   &>>${log_file}
-systemctl enable dispatch   &>>${log_file}
-systemctl restart dispatch    &>>${log_file}
+systemctl enable ${component}   &>>${log_file}
+systemctl restart ${component}    &>>${log_file}
 
